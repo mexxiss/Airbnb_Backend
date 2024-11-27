da420474ea5058188c204f133ff8ec22a30f9816
amansekhon-cloud <amansekhon.mexxiss@gmail.com>
2024-11-27 12:10:27 +0530

    Booked Dates Month-wise data filter Endpoint

--- controllers/BookedDates.js
+++ controllers/BookedDates.js
@@ -6,23 +6,44 @@ import { UtilityModel } from "../models/Utility.js";
 import mongoose from "mongoose";
 
 export const GetFilteredDates = async (req, res, next) => {
-    const { start_date, end_date, property } = req.body;
+    const { start_date, property } = req.body;
 
-    if (!property) {
+    if (!property && mongoose.isValidObjectId(property)) {
         return next(new apiError(400, "Property ID not provided"));
     }
 
-    if (!start_date || !end_date || start_date >= end_date) {
-        return next(new apiError(400, "Invalid Date"));
-    }
+    const s_date = start_date ? new Date(start_date) : new Date(new Date().getFullYear(), new Date().getMonth(), 1);
 
+    const e_date = new Date(s_date);
+    e_date.setMonth(e_date.getMonth() + 4);
+    
     try {
         const dates = await BookedDatesModel.aggregate([
-            { $match: {
-                property: new mongoose.Types.ObjectId(property),
-                checkin_date: { $lte: new Date(start_date) },
-                checkout_date: { $gte: new Date(end_date)}
-            } }
+            {
+                $match: {
+                    property: new mongoose.Types.ObjectId(property),
+                    $expr: {
+                        $and: [
+                            { $gte: ["$checkout_date", s_date] },
+                            { $lte: ["$checkin_date", e_date] }
+                        ]
+                    },
+                }
+            },
+            {
+                $group: {
+                    _id: { month: {$month: "$checkout_date"}, year: {$year: "$checkout_date"} },
+                    total_stays: { $sum: 1 },
+                    total_nights_count: { $sum: "$nights_count" },
+                    total_stay_charges: { $sum: "$cost_details.stay_charges" },
+                    total_cleaning_fee: { $sum: "$cost_details.cleaning_fee" },
+                    total_discount: { $sum: "$cost_details.discount" },
+                    total_tourism_tax: { $sum: "$cost_details.tourism_tax" },
+                    total_vat_tax: { $sum: "$cost_details.vat_tax" },
+                    total_net_charges: { $sum: "$cost_details.net_charges" },
+                    documents: {$push: "$$ROOT"}
+                }
+            },
         ]);
         return res.status(200).json(new apiResponse(200, dates, "Dates Retrieved Successfully"));
     } catch (error) {
