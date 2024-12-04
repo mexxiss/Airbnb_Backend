4c76815d5b0b5c7162aba96ab8794180b18c39de
amansekhon-cloud <amansekhon.mexxiss@gmail.com>
2024-12-04 10:53:03 +0530

    User Update Controller

--- routes/Users.js
+++ routes/Users.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { Auth } from "../middleware/auth.js";
-import { ChangePassword, Login, Logout, SignUp } from "../controllers/Users.js";
+import { ChangePassword, Login, Logout, SignUp, UpdateUser } from "../controllers/Users.js";
 
 const router = express.Router();
 
@@ -8,5 +8,6 @@ router.post('/signup', SignUp)
 router.post('/login', Login)
 router.post('/logout', Auth, Logout)
 router.post('/change-pass', Auth, ChangePassword);
+router.put('/', Auth, UpdateUser);
 
 export default router
