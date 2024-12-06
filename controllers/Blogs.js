5e235d4f2202a709f8451eaab391c36e85983fa3
amansekhon-cloud <amansekhon.mexxiss@gmail.com>
2024-12-06 11:22:13 +0530

    Blog by ID and customized response

--- controllers/Blogs.js
+++ controllers/Blogs.js
@@ -1,3 +1,4 @@
+import mongoose from "mongoose";
 import { BlogsModel } from "../models/Blogs.js";
 import { apiError } from "../utils/apiError.js";
 import { apiResponse } from "../utils/apiResponse.js";
@@ -31,6 +32,31 @@ export const GetBlogs = async (req, res, next) => {
     }
 };
 
+export const GetBlog = async (req, res, next) => {
+    const { id } = req.params;
+
+    if (!id || !mongoose.isValidObjectId(id)) {
+        return next(new apiError(400, "Blog id not provided or invalid"));
+    }
+
+    try {
+        const blog = await BlogsModel.findById(id);
+        if (!blog) {
+            return next(new apiError(404, "Blog not found"));
+        }
+
+        const relatedBlogs = await BlogsModel.find({
+            category: blog.category,
+            _id: { $ne: id },
+        }).select("title subtitle added_on thumbnail").limit(6); 
+
+        return res.status(200).json(
+            new apiResponse(200, { blog, relatedBlogs }, "Blog fetched successfully")
+        );
+    } catch (error) {
+        return next(new apiError(500, `Server Error: ${error.message}`));
+    }
+};
 
 export const AddBlogs = async (req, res, next) => {
     const { title, subtitle, body, tags, added_on, category, thumbnail } = req.body;
