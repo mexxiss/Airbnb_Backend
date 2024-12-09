a2e4917eb2a249678748e6a2b59498e73f22e32b
amansekhon-cloud <amansekhon.mexxiss@gmail.com>
2024-12-09 17:44:45 +0530

    update user documents

--- controllers/UserDocuments.js
+++ controllers/UserDocuments.js
@@ -33,3 +33,36 @@ export const GetUserDocuments = async (req, res, next) => {
         return next(new apiError(500, `Server Error: ${error}`))
     }
 }
+
+export const UpdateUserDocuments = async (req, res, next) => {
+    const {id} = req.params;
+    const user_id = req._id;
+    const {updates} = req.body;
+
+    if (!updates) {
+        return next(new apiError(400, `Payment Details Update required`));
+    }
+    if( !user_id ) {
+        return next(new apiError(400, `User required`));
+    }
+
+    try {
+        const doc = await UserDocumentsModel.findOne({ _id: id });
+
+        if (!doc) {
+            return next(new apiError(404, `Payment Details not found`));
+        }
+
+        if (user_id !== doc.user.toString()) {
+            return next(new apiError(403, `Not Authorized to change details`));
+        }
+
+        doc.set(updates);
+        await doc.save({ validateBeforeSave: true });
+
+        return res.status(200).json(doc);
+    } catch (error) {
+        console.error(`Error updating payment details: ${error.message}`);
+        return next(new apiError(500, `Server Error`));
+    }
+}
