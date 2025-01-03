e2b019a5508aae1a54e3a9d48ca71e592f310419
aman-sekhon-mexxiss <amansekhon.mexxiss@gmail.com>
2025-01-03 10:06:04 +0530

    Update estimaterevenue.controllers.js

--- controllers/general/content/estimaterevenue.controllers.js
+++ controllers/general/content/estimaterevenue.controllers.js
@@ -6,47 +6,15 @@ export const GetAreas = async (req, res, next) => {
     // #swagger.tags = ['General']
     // #swagger.summary = 'Endpoint to get areas & bedrooms to map on website's UI - Estimate Revenue Page'
 
-    const regions = [
-        "Al Barsha South",
-        "Al Furjan",
-        "Al Jaddaf",
-        "Arabian Ranches (Villas)",
-        "Bluewaters Island",
-        "Business Bay",
-        "Citywalk",
-        "DAMAC Hills (Apartments)",
-        "DAMAC Hills (Villas)",
-        "DIFC",
-        "Discovery Gardens",
-        "Downtown Dubai",
-        "Dubai Hills",
-        "Dubai South",
-        "Festival City",
-        "Greens",
-        "JVT / JVC",
-        "Jumeirah Beach Residences (JBR)",
-        "Jumeirah Lake Towers (JLT)",
-        "MBR City",
-        "Madinat Jumeirah",
-        "Marina",
-        "Meydan",
-        "Palm Jumeirah",
-        "Ras Al Khor",
-        "Silicon Oasis",
-        "Sports City/IMPZ",
-        "Springs (Villas)",
-        "Tecom"
-    ];
-    const bedrooms = [
-        "One Bed",
-        "Two Bed",
-        "Three Bed",
-        "Four Bed",
-        "Studio",
-        "Five Bed",
-        "Six Bed"
-    ]
-    return res.status(200).json({ regions, bedrooms })
+    try {
+        const regions = await EstimateRevenueModel.find();
+        if (!regions) {
+            return res.status(404).json({ message: "No regions found." });
+        }
+        return res.status(200).json(regions);
+    } catch (e) {
+        return next(apiError(res, 500, "Server error fetching regions."));
+    }
 }
 
 export const GetEstimateRevenueList = async (req, res) => {
